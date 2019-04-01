aebf90dddf63545fa891839a1e3ddef7ce0ab790
Nikhil K C <nikhilkc96@gmail.com>
2019-04-01 23:19:03 +0530

    Removed the webview

--- app/src/main/java/com/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/qrcodescanner/MainActivity.java
@@ -2,10 +2,12 @@ package com.qrcodescanner;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.webkit.WebSettings;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -46,9 +48,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonbatch.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, BatchActivity.class);
 
-                startActivity(intent);
+                SharedPreferences sp = getSharedPreferences("batch", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                String s1= sp.getString("batchid",null);
+                // webView.loadUrl("http://35.200.186.171/supplychain-frontend/view-batch.php?batchNo="+s1);
+
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://35.200.186.171/supplychain-frontend/view-batch.php?batchNo="+s1));
+                startActivity(i);
             }
         });
     }
